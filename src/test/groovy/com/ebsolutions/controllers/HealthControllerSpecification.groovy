04f72f275cbde75c87d6051d9d9dc11bcc2bbf83
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-02-16 20:12:51 -0500

    Need to resolve the NPE from client

--- src/test/groovy/com/ebsolutions/controllers/HealthControllerSpecification.groovy
+++ src/test/groovy/com/ebsolutions/controllers/HealthControllerSpecification.groovy
@@ -1,15 +1,23 @@
 package com.ebsolutions.controllers
 
+import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpStatus
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import jakarta.inject.Inject
 import spock.lang.Specification
+import spock.lang.Unroll
 
 class HealthControllerSpecification extends Specification {
-    def "Health controller should return correct response"() {
-        given: "A health controller is running"
-        HealthController healthController = new HealthController();
+    @Inject
+    @Client('/')
+    HttpClient client
 
-        when: "it is invoked"
-        HttpStatus response = healthController.index()
+    @Unroll
+    void "Health controller should return correct response"() {
+
+        when: "the health endpoint is invoked"
+        HttpStatus response = client.toBlocking().retrieve(HttpRequest.GET("/health"), HttpStatus)
 
         then: "the it should return the correct response"
         response == HttpStatus.OK
