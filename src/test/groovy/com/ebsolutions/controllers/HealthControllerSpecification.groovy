97aadfaf8cc728f7b7c3b4bcfe65c7c5a7c5cfcb
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2023-02-20 09:35:18 -0500

    Adding in the gradle deps and the health controller test

--- src/test/groovy/com/ebsolutions/controllers/HealthControllerSpecification.groovy
+++ src/test/groovy/com/ebsolutions/controllers/HealthControllerSpecification.groovy
@@ -4,10 +4,12 @@ import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpStatus
 import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
 import spock.lang.Specification
 import spock.lang.Unroll
 
+@MicronautTest
 class HealthControllerSpecification extends Specification {
     @Inject
     @Client('/')
@@ -22,4 +24,6 @@ class HealthControllerSpecification extends Specification {
         then: "the it should return the correct response"
         response == HttpStatus.OK
     }
+
+
 }
